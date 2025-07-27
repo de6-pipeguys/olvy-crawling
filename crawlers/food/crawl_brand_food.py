504631504719993ef55852e470c5387dbcbfe73b
westg <kwkimsk0630@naver.com>
2025-07-27 15:22:57 +0900

    feat: food와 healthcare용 크롤링 함수 수정

--- crawlers/food/crawl_brand_food.py
+++ crawlers/food/crawl_brand_food.py
@@ -6,7 +6,6 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import logging
 import json
 import os
 
